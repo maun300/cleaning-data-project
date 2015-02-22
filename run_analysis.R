e3fad94805bb0b5b3c6b9b0f50d151f87c7835d8
Markus Undhagen <markus.undhagen@gmail.com>
2015-02-22 10:22:13 +0100

    Last fixes

--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,25 @@
-# prereq install.packages("dplyr"), 
+# The file contains the functions needed to create the tidy data set that is used
+# in the project for the Coursera course Getting and cleaning data.
 
+# Downloads the zip file with the data and unzips it in the directory "working directory/data"
+# The directory data is created if it not exists.
 downloadData <- function() {
+    # Check if data directory exists. Creat it if it's not.
     if (!file.exists("data")) {
         dir.create("data")
     }
 
+    # Down load the file
     fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
     download.file(fileUrl, destfile = "./data/dataset.zip", method = "curl")
     dateDownloaded <- date()
 
+    # unzip the file in the data directory 
     unzip("./data/dataset.zip", exdir="./data")
 }
 
+# Read observations, subject and activity from files into one data frame. The
+# parameter name is either "test" to read test data or "train" to read the training data
 getData <- function(name) {
     # read observations into dataframe
     filename <- paste0("./data/UCI HAR Dataset/", name, "/X_", name, ".txt")
@@ -30,56 +38,50 @@ getData <- function(name) {
     cbind(data, subjectid)
 }
 
-# fix test data
-testdata <- getData("test")
-#testdata <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
-#testlabels <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
-#testsubjects <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
-#testdata <- cbind(testdata, testlabels)
-#testdata <- cbind(testdata, testsubjects)
-#testlabels <- NULL
-#testsubjects <- NULL
+# The function creates the tidy data set from the test and training data sets
+createTidyDataset <- function() {
+    # read the test data, subject and activity
+    testdata <- getData("test")
 
-#fix train data
-traindata <- getData("train")
-#traindata <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
-#trainlabels <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
-#trainsubjects <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
-#traindata <- cbind(traindata, trainlabels)
-#traindata <- cbind(traindata, trainsubjects)
-#trainlabels <- NULL
-#trainsubjects <- NULL
+    #read the training data, subject and activity
+    traindata <- getData("train")
 
-data <- rbind(traindata, testdata)
-traindata <- NULL
-testdata <- NULL
+    ## combine the data into one data frame
+    data <- rbind(traindata, testdata)
 
-# set column names from the file features.txt + add the names for activity and subject
-colnames <- read.table("./data/UCI HAR Dataset/features.txt")
-colnames(data) <- colnames[,2]
-colnames(data)[562] <- "activityid"
-colnames(data)[563] <- "subjectid"
+    # set column names from the file features.txt + add the names for activity and subject
+    colnames <- read.table("./data/UCI HAR Dataset/features.txt")
+    colnames(data) <- colnames[,2]
+    colnames(data)[562] <- "activityid"
+    colnames(data)[563] <- "subjectid"
 
-# only keep columns with std() or mean() in the name and activity and subject
-cols <- append(grep("(mean|std)\\(\\)",colnames(data)), 562:563)
-data <- data[cols]
-colnames <- NULL
+    # only keep columns with std() or mean() in the name and activity and subject
+    cols <- append(grep("(mean|std)\\(\\)",colnames(data)), 562:563)
+    data <- data[cols]
 
-# add column with the activty names 
-activitylabels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
-colnames(activitylabels) <-(c("activityid", "activity"))
-data <- merge(data, activitylabels, by.x="activityid", by.y="activityid", all.x)
+    # add column with the activty names 
+    activitylabels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+    colnames(activitylabels) <-(c("activityid", "activity"))
+    data <- merge(data, activitylabels, by.x="activityid", by.y="activityid", all.x)
+    
+    #remove the column activityid
+    data <- data[, !(names(data) %in% "activityid")]
+    
+    # cleanup column names
+    colnames(data) <- make.names(names(data), unique=TRUE)
+    colnames(data) <- gsub("\\.", "", names(data))
 
-#remove the column activityid
-data <- data[, !(names(data) %in% "activityid")]
+    # create final dataset with the with the average of each variable for each activity and each subject.
+    finaldata <- group_by(data, activity, subjectid)
+    finaldata <- summarise_each(finaldata, funs(mean))
+    
+    # save dataset to file
+    write.table(finaldata, "./data/finaldata.txt", row.name=FALSE)    
+}
 
-# cleanup column names
-colnames(data) <- make.names(names(data), unique=TRUE)
-colnames(data) <- gsub("\\.", "", names(data))
 
-# create final dataset with the with the average of each variable for each activity and each subject.
-finaldata <- group_by(data, activity, subjectid)
-finaldata <- summarise_each(finaldata, funs(mean))
 
-# save dataset to file
-write.table(finaldata, "./data/finaldata.txt", row.name=FALSE)
+
+
+
+
